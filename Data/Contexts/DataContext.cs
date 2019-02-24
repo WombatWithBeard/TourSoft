621fb0402c2523f0c9ad3d617eed5bdf979e31f1
WombatWithBeard <boroda132@yandex.ru>
2019-02-24 15:43:17 +0300

    for migration

--- Data/Contexts/DataContext.cs
+++ Data/Contexts/DataContext.cs
@@ -7,39 +7,41 @@ namespace ToursSoft.Data.Contexts
  {
      public class DataContext: DbContext
      {
-         public DbSet<Excursion> Excursions { get; set; }
 
-         public DbSet<Person> Persons { get; set; }
+        public DbSet<Excursion> Excursions { get; set; }
          
-         public DbSet<Hotel> Hotels { get; set; }
+        public DbSet<Hotel> Hotels { get; set; }
 
-         public DbSet<Tour> Tours { get; set; }
+        public DbSet<Tour> Tours { get; set; }
 
-         public DbSet<ManagersGroup> ManagersGroups { get; set; }
+        public DbSet<ManagersGroup> ManagersGroups { get; set; }
 
-         public DbSet<TourPrice> TourPrices { get; set; }
+        public DbSet<TourPrice> TourPrices { get; set; }
 
-         public DbSet<User> Users { get; set; }
+        public DbSet<User> Users { get; set; }
 
-         public DataContext(DbContextOptions<DataContext> options):base(options)
+        public DbSet<Person> Persons { get; set; }
+
+        public DataContext(DbContextOptions<DataContext> options):base(options)
          {
          }
          
-         public DataContext()
+        public DataContext()
          { 
          }
 
-         protected override void OnModelCreating(ModelBuilder modelBuilder)
-         {
-             modelBuilder.Entity<User>().ToTable("User");
-             modelBuilder.Entity<Tour>().ToTable("Tour");
-             modelBuilder.Entity<Hotel>().ToTable("Hotel");
-             modelBuilder.Entity<TourPrice>().ToTable("TourPrice");
-             modelBuilder.Entity<ManagersGroup>().ToTable("ManagersGroup");
-             modelBuilder.Entity<Excursion>().ToTable("Excursion");
-         }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>().ToTable("User");
+            modelBuilder.Entity<Tour>().ToTable("Tour");
+            modelBuilder.Entity<Hotel>().ToTable("Hotel");
+            modelBuilder.Entity<TourPrice>().ToTable("TourPrice");
+            modelBuilder.Entity<ManagersGroup>().ToTable("ManagersGroup");
+            modelBuilder.Entity<Excursion>().ToTable("Excursion");
+            modelBuilder.Entity<Person>().ToTable("Person");
+        }
 
-//         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-//             => optionsBuilder.UseNpgsql(@"host=localhost;port=5432;database=tours;username=postgres;password=postgres");
-     }
+        //         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        //             => optionsBuilder.UseNpgsql(@"host=localhost;port=5432;database=tours;username=postgres;password=postgres");
+    }
  }
